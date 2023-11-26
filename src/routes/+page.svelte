ce7426f7b7a2435ad5609c0499b48ccb0df9208e
manish-mehra <manishmehra1469@gmail.com>
2023-11-26 14:19:21 +0530

    refactor code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
  import '../tailwind.css'
+ import {PROJECTS, API_URL} from '../utils/constants'
  
- const API_URL = 'https://dev.to/api/articles?username=manishmehra'
-
+ 
  let currentTab = 'home'
  let loading = false
  let blogs = []
@@ -55,7 +55,7 @@
         <div class="text-center pb-10">
           <h1 class="text-3xl font-bold">Hi!👋🏻</h1>
           <h1 class="text-3xl font-bold pb-3">I'm Manish Mehra</h1>
-          <p class="text-sm text-gray-400 font-medium">An <span class="font-bold">aspiring</span> software engineer with hands-on experience in <span class="underline decoration-wavy decoration-red-900">frontend technologies</span></p>
+          <p class="text-md text-gray-400 font-medium">An <span class="font-bold">aspiring</span> software engineer with hands-on experience in <span class="underline decoration-wavy decoration-red-900">frontend technologies</span></p>
         </div>
         <div class="flex gap-2">
           <a href = "https://github.com/manish-mehra" class="bg-[#2a2a2abf] px-4 py-1 rounded-md text-sm font-medium" target="_blank">Github</a>
@@ -64,43 +64,25 @@
         </div>
       </section>
       {:else if currentTab === 'projects'}
-        <section class="flex flex-col items-center gap-6 mt-16">
-            <figure class="max-w-2xl w-full p-4 rounded-md bg-[#242424] flex flex-col items-start">
-              <div class="pb-3">
-                <h3 class="text-xl font-bold">LiteWriteNeo <span class="text-gray-600">#WIP</span></h3>
-              </div>
-              <div class="mb-5">
-                <p class="text-sm font-normal text-gray-400">LiteWriteNeo is an open-source distraction free writing web app, rewritten in pure JavaScript with TypeScript for enhanced performance and maintainability</p>
-              </div>
-              <div>
-                <a href="https://github.com/manish-mehra/litewriteneo" target="_blank" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Github</a>
-              </div>
-            </figure>
-            <figure class="max-w-2xl w-full p-4 rounded-md bg-[#2a2a2abf] flex flex-col items-start">
-              <div class="pb-3">
-                <h3 class="text-xl font-bold">LocShare</h3>
-              </div>
-              <div class="mb-5 flex flex-col gap-4 overflow-hidden">
-                <p class="text-sm font-normal text-gray-400">Realtime location sharing: React, Sockets, and Leaflet</p>
-                <img src="https://user-images.githubusercontent.com/31545725/271251815-66d550b7-a7bf-4c3c-9f57-2802097b133c.png" alt="" class="rounded-md">
-              </div>
-              <div>
-                <a href="https://github.com/manish-mehra/locshare" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Github</a>
-              </div>
-            </figure>
-            <figure class="max-w-2xl w-full p-4 rounded-md bg-[#2a2a2abf] flex flex-col items-start">
-              <div class="pb-3">
-                <h3 class="text-xl font-bold">HindustanHeadlines</h3>
-              </div>
-              <div class="mb-5 flex flex-col gap-4 overflow-hidden">
-                <p class="text-sm font-normal text-gray-400">A news headlines curator based on webscraping | ReactJS & Node</p>
-                <img src="https://raw.githubusercontent.com/manish-mehra/hindustan-headlines/main/client/public/hindustan-headlines-img.png" alt="hindustan headlines" class="rounded-md"/>
-              </div>
-              <div>
-                <a href="https://github.com/manish-mehra/hindustan-headlines" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Github</a>
-              </div>
-            </figure>
-        </section>
+      <section class="flex flex-col items-center gap-6 mt-16">
+        {#each PROJECTS as project}
+          <figure class="max-w-2xl w-full p-4 rounded-md bg-[#242424] flex flex-col items-start">
+            <div class="pb-3">
+              <h3 class="text-xl font-bold">{project.name}</h3>
+            </div>
+            <div class="mb-5 flex flex-col gap-4 overflow-hidden">
+              <p class="text-md font-normal text-gray-400">{project.description}</p>
+              {#if project.image}
+                <img src={project.image} alt={project.name} class="rounded-md"/>
+              {/if}
+            </div>
+            <div>
+              <a href={project.github} target="_blank" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Github</a>
+            </div>
+          </figure>
+        {/each}
+      </section>
+
       {:else if currentTab === 'blogs'}
       <section class="flex flex-col items-center gap-6 mt-16">
         {#if loading}
@@ -109,30 +91,30 @@
 
         {#if blogs.length > 0}
           {#each blogs as blog(blog.id)}
-          <figure class="max-w-2xl w-full p-4 rounded-md bg-[#242424] flex flex-col items-start">
-            <div class="pb-3">
-              <h3 class="text-xl font-bold">{blog?.title}</h3>
-            </div>
-            <div class="mb-5">
-              <p class="text-sm font-normal text-gray-400">
-                Published on: {blog?.readable_publish_date}<br/>
-                Author: {blog?.user?.name}<br/>
-                Reading Time: {blog?.reading_time_minutes}<br/>
-              </p>
-            </div>
-            <div>
-              <a href={blog?.url} target="_blank"  class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Read</a>
-            </div>
-          </figure>
+            <a href={blog?.url} target="_blank" class="max-w-2xl w-full p-4 cursor-pointe rounded-md bg-[#242424] flex flex-col items-start">
+              <div class="pb-3">
+                <h3 class="text-xl font-bold">{blog?.title}</h3>
+              </div>
+              <div class="mb-5">
+                <p class="text-md font-normal text-gray-400">
+                  Published on: {blog?.readable_publish_date}<br/>
+                  Author: {blog?.user?.name}<br/>
+                  Reading Time: {blog?.reading_time_minutes}<br/>
+                </p>
+              </div>
+              <div>
+                <a href={blog?.url} target="_blank"  class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Read</a>
+              </div>
+            </a>
           {/each}
         {/if}
 
       </section>
       {:else if currentTab === 'contact'}
-      <section class="flex flex-col items-center gap-6 mt-16">
-        <div>
-          <h1 class="text-3xl font-bold mb-4">Feel free to reach me out!</h1>
-          <div class="text-sm font-medium flex flex-col gap-2">
+      <section class="flex flex-col gap-6 mt-16">
+        <div class="">
+          <h1 class="text-xl font-bold mb-4">Feel free to reach me out!</h1>
+          <div class="text-md font-medium flex flex-col gap-2">
             <p>📧 Write a <a href="mailto:itsmanishmehra@gmail.com" target="_blank" class="underline">mail</a></p>
             <p>📱 Connect on <a href="https://www.linkedin.com/in/mr-manish-mehra/" target = "_blank" class="underline">Linkedln</a></p>
             <p>🐦 Connect on <a href="https://twitter.com/_manishmehra" target="_blank" class="underline">Twitter</a></p>
---
+++ src/utils/constants/index.js
@@ -0,0 +1,23 @@
+
+export const API_URL = 'https://dev.to/api/articles?username=manishmehra'
+
+export const PROJECTS = [
+  {
+    name: 'LiteWriteNeo',
+    description: 'LiteWriteNeo is an open-source distraction free writing web app, rewritten in pure JavaScript with TypeScript for enhanced performance and maintainability',
+    github: 'https://github.com/manish-mehra/litewriteneo',
+    image: ''
+  },
+  {
+    name: 'LocShare',
+    description: 'Realtime location sharing: React, Sockets, and Leaflet',
+    github: 'https://github.com/manish-mehra/locshare',
+    image: 'https://user-images.githubusercontent.com/31545725/271251815-66d550b7-a7bf-4c3c-9f57-2802097b133c.png'
+  },
+  {
+    name: 'HindustanHeadlines',
+    description: 'A news headlines curator based on webscraping | ReactJS & Node',
+    github: 'https://github.com/manish-mehra/hindustan-headlines',
+    image: 'https://raw.githubusercontent.com/manish-mehra/hindustan-headlines/main/client/public/hindustan-headlines-img.png'
+  }
+ ]
