bc4f4579965bf1c5a7f59c822c88eff05490ee7a
manish-mehra <manishmehra1469@gmail.com>
2023-12-10 23:50:02 +0530

    add books page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,15 +9,16 @@
 </script>
 
 <main class="min-h-screen h-full w-full bg-[#121212] pb-4">
-  <header class="fixed top-0 z-10 mt-4 mb-4 mx-auto bg-[#242424]/80 backdrop-blur  border-slate-900 rounded-full left-0 right-0 w-fit">
-    <section class="text-white text-lg font-medium p-3 sm:px-12 flex justify-center gap-3 sm:gap-5">
-      <a href="/" class={`${currentPath === '/' && 'text-red-600'} px-2 cursor-pointer`} >Home</a>
-      <a href="/projects" class={`${currentPath === 'projects' && 'text-red-600'} px-2 cursor-pointer`}>Projects</a>
-      <a href = "/blogs" class={`${currentPath === 'blogs' && 'text-red-600'} px-2 cursor-pointer`}>Blogs</a>
-      <a href = "/contact" class={`${currentPath === 'contact' && 'text-red-600'} px-2 cursor-pointer`}>Contact</a>
-    </section>
-  </header>
-  <div class="pt-20 p-3 text-white flex items-center justify-center ">
+    <header class="fixed top-0 left-0 right-0 z-10 mt-4 mb-4 mx-auto px-3 flex items-center justify-center overflow-hidden">
+      <section class="bg-[#242424]/80 backdrop-blur border-slate-900 drop-shadow-lg rounded-full text-white text-lg font-medium p-3 sm:px-12 flex gap-2 sm:gap-5 overflow-auto">
+        <a href="/" class={`${currentPath === '/' && 'text-red-600'} px-2 cursor-pointer`} >Home</a>
+        <a href="/projects" class={`${currentPath === 'projects' && 'text-red-600'} px-2 cursor-pointer`}>Projects</a>
+        <a href = "/blogs" class={`${currentPath === 'blogs' && 'text-red-600'} px-2 cursor-pointer`}>Blogs</a>
+        <a href = "/books" class={`${currentPath === 'books' && 'text-red-600'} px-2 cursor-pointer`}>Books</a>
+        <a href = "/contact" class={`${currentPath === 'contact' && 'text-red-600'} px-2 cursor-pointer`}>Contact</a>
+      </section>
+    </header>
+  <div class="pt-20 p-3 text-white flex items-center justify-center my-0 mx-auto max-w-2xl">
     <slot/>
   </div>
 </main>
---
+++ src/routes/books/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+  import Books from '../../utils/constants/books.json'
+
+  const tags = ['All', "Currently Reading", ...new Set(Books.map(book => book.tags).flat())]
+
+  const currentlyReading = [
+    {
+      title: 'The Count of Monte Cristo',
+      author: 'Alexandre Dumas',
+      pages: 932,
+      tags: ['classics', 'fiction']
+    },
+    {
+      title: 'Thinking Fast and Slow',
+      author: 'Daniel Kahneman',
+      pages: 499,
+      tags: ['psychology', 'non-fiction']
+    }
+  ]
+
+  let books = Books
+  let currentTag = 'All'
+
+</script>
+
+<svelte:head>
+    <link rel="shortcut icon" href="/favicon.ico">
+    <title>Books - Manish Mehra</title>
+    <meta name="description" content="Explore Manish Mehra's projects in software engineering">
+</svelte:head>
+
+<div class="flex flex-col mt-16">
+  <section class="mb-10">
+    <h2 class="text-lg font-semibold">💭I'm an occasional bookworm and over the past few years, I've read these books. I highly recommend them.</h2>
+  </section>
+
+  <section class="mb-5">
+    <div class="flex flex-wrap gap-2">
+      {#each tags as tag}
+      <button 
+        class= {`
+          text-sm rounded-full px-3 py-1 font-medium 
+          ${currentTag === tag ? 'bg-gray-200 text-black' : 'bg-gray-800 text-white'}
+        `}
+        on:click={() => {
+          currentTag = tag
+          if(tag === 'All'){
+            books = Books
+          } else if(tag === 'Currently Reading') {
+            books = currentlyReading
+          } else {
+            books = Books.filter(book => book.tags.includes(tag))
+          }
+        }}
+      >{tag}</button>
+    {/each}
+    </div>
+  </section>
+  <section class="mb-5">
+   <div class="flex gap-2 items-center justify-end font-medium">
+      <p>{books.length} Books</p> |
+      <p>{
+      books.reduce((acc, book) => {
+        return acc + book.pages
+      }, 0)} Pages</p>
+   </div>
+  </section>
+  <section class="flex flex-col items-center gap-6">
+    {#each books as book}
+      <figure class="max-w-2xl w-full p-4 rounded-md bg-[#242424] flex flex-col items-start">
+        <div class="pb-3">
+          <h3 class="text-xl font-bold">{book.title}
+          </h3>
+        </div>
+        <div class="mb-5 w-full flex flex-col gap-4 overflow-hidden">
+            <p class="text-md font-normal text-gray-400">{book.author}</p>
+          <div class="flex flex-wrap items-center gap-2">
+            {#each book.tags as tag}
+            <span class="text-sm rounded-full px-3 py-1 font-medium bg-gray-200 text-black">{tag}</span>
+          {/each}
+          </div>
+          <div class="flex w-full items-end justify-end">
+            <p class="text-md font-normal text-gray-400 text-xs">{book.pages} pages</p>
+          </div>
+        </div>
+        <div>
+        </div>
+      </figure>
+    {/each}
+  </section>
+</div>
