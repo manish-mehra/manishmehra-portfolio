26cc9dcc0c1cbe6b4bcd5ed9c9385e6bf8c27117
manish-mehra <manishmehra1469@gmail.com>
2024-03-19 09:48:50 +0530

    add project

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -12,11 +12,11 @@
   {#each PROJECTS as project}
     <figure class="max-w-2xl w-full p-4 rounded-md bg-[#242424] flex flex-col items-start">
       <div class="pb-3">
-        <h3 class="text-xl font-bold">{project.name}
-          {#if project.name === 'LiteWriteNeo'}
-            <span class="text-lg font-bold text-gray-400"> (#WIP)</span>
-          {/if}
-        </h3>
+        {#if project.name === 'https://ieltswriting.app/'}
+          <a href={project.name} class="text-xl font-bold">{project.name}</a>
+        {:else}
+          <h3 class="text-xl font-bold">{project.name}</h3>
+        {/if}
       </div>
       <div class="mb-5 flex flex-col gap-4 overflow-hidden">
         <p class="text-md font-normal text-gray-400">{project.description}</p>
@@ -25,7 +25,12 @@
         {/if}
       </div>
       <div>
+        {#if project.github}
         <a href={project.github} target="_blank" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Github</a>
+        {/if}
+        {#if project.link}
+        <a href={project.link} target="_blank" class="bg-[#2a2a2abf] border px-4 py-1 rounded-md text-sm font-medium">Website</a>
+        {/if}
       </div>
     </figure>
   {/each}
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -3,21 +3,30 @@ export const API_URL = 'https://dev.to/api/articles?username=manishmehra'
 
 export const PROJECTS = [
   {
-    name: 'LiteWriteNeo',
-    description: 'LiteWriteNeo is an open-source distraction free writing web app, rewritten in pure JavaScript with TypeScript for enhanced performance and maintainability',
-    github: 'https://github.com/manish-mehra/litewriteneo',
-    image: ''
+    name: 'https://ieltswriting.app/',
+    description: 'A web app designed as a practical solution, actively used by students in their IELTS exam writing preparation. A modern full stack app, built with Next.js, Typescript, and TailwindCSS.',
+    github: '',
+    link: 'https://ieltswriting.app/',
+    image: '/ielts-writing-app.png'
   },
+  // {
+  //   name: 'LiteWriteNeo',
+  //   description: 'LiteWriteNeo is an open-source distraction free writing web app, rewritten in pure JavaScript with TypeScript for enhanced performance and maintainability',
+  //   github: 'https://github.com/manish-mehra/litewriteneo',
+  //   image: ''
+  // },
   {
     name: 'LocShare',
     description: 'Realtime location sharing: React, Sockets, and Leaflet',
     github: 'https://github.com/manish-mehra/locshare',
+    link: '',
     image: 'https://user-images.githubusercontent.com/31545725/271251815-66d550b7-a7bf-4c3c-9f57-2802097b133c.png'
   },
-  {
-    name: 'HindustanHeadlines',
-    description: 'A news headlines curator based on webscraping | ReactJS & Node',
-    github: 'https://github.com/manish-mehra/hindustan-headlines',
-    image: 'https://raw.githubusercontent.com/manish-mehra/hindustan-headlines/main/client/public/hindustan-headlines-img.png'
-  }
+  // {
+  //   name: 'HindustanHeadlines',
+  //   description: 'A news headlines curator based on webscraping | ReactJS & Node',
+  //   github: 'https://github.com/manish-mehra/hindustan-headlines',
+  //   link: '',
+  //   image: 'https://raw.githubusercontent.com/manish-mehra/hindustan-headlines/main/client/public/hindustan-headlines-img.png'
+  // }
  ]
